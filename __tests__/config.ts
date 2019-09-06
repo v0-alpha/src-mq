0dfe206d63dd74bdc598ab95f0e4f1231840ba89
Alex Sanders <a@snd.rs>
2019-09-06 18:16:14 +0100

    one test obj

--- __tests__/config.ts
+++ __tests__/config.ts
@@ -1,18 +1,18 @@
 import { breakpoints, resetBreakpoints, setBreakpoints } from '../src/config'
 
+const defaults = {
+	xxSmall: expect.any(Number),
+	xSmall: expect.any(Number),
+	small: expect.any(Number),
+	medium: expect.any(Number),
+	large: expect.any(Number),
+	xLarge: expect.any(Number),
+	xxLarge: expect.any(Number),
+}
+
 describe('breakpoints', () => {
 	test('are defaults without doing anything', () => {
-		expect(breakpoints).toEqual(
-			expect.objectContaining({
-				xxSmall: expect.any(Number),
-				xSmall: expect.any(Number),
-				small: expect.any(Number),
-				medium: expect.any(Number),
-				large: expect.any(Number),
-				xLarge: expect.any(Number),
-				xxLarge: expect.any(Number),
-			}),
-		)
+		expect(breakpoints).toEqual(expect.objectContaining(defaults))
 	})
 	test('can be updated by user', () => {
 		setBreakpoints({ tiny: 5 })
@@ -22,16 +22,6 @@ describe('breakpoints', () => {
 	test('can be reset to default', () => {
 		setBreakpoints({ tiny: 5 })
 		resetBreakpoints()
-		expect(breakpoints).toEqual(
-			expect.objectContaining({
-				xxSmall: expect.any(Number),
-				xSmall: expect.any(Number),
-				small: expect.any(Number),
-				medium: expect.any(Number),
-				large: expect.any(Number),
-				xLarge: expect.any(Number),
-				xxLarge: expect.any(Number),
-			}),
-		)
+		expect(breakpoints).toEqual(expect.objectContaining(defaults))
 	})
 })
