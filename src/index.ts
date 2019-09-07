23ec8ae1852aa4b207bb962c499efa95b4b07ef1
Alex Sanders <a@snd.rs>
2019-09-07 02:05:17 +0100

    add froms/untils
    but autocompletes aren't working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
-import { resetBreakpoints, setBreakpoints } from './config'
+import { breakpoints, resetBreakpoints, setBreakpoints } from './config'
+import { fromQuery, fromUntilQuery, untilQuery } from './media-queries'
+
+type Breakpoints = keyof typeof breakpoints
+
+type Until = { [key in Breakpoints]: string }
+
+type From = {
+	[key in Breakpoints]: {
+		toString: () => string
+		until: Until
+	}
+}
+
+export const until = Object.entries(breakpoints).reduce(
+	(untils, [untilName, untilWidth]) => ({
+		[untilName]: untilQuery(untilWidth),
+		...untils,
+	}),
+	{},
+) as Until
+
+export const from = Object.entries(breakpoints).reduce(
+	(froms, [fromName, fromWidth], i) => ({
+		[fromName]: {
+			until: Object.entries(breakpoints)
+				.splice(i + 1)
+				.reduce(
+					(untils, [untilName, untilWidth], i) => ({
+						[untilName]: fromUntilQuery(fromWidth, untilWidth),
+						...untils,
+					}),
+					{},
+				),
+			toString: () => fromQuery(fromWidth),
+		},
+		...froms,
+	}),
+
+	{},
+) as From
 
 export { resetBreakpoints, setBreakpoints }
