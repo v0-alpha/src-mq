4c6a3494ca5149a36db24a89520c8605b98c360c
Alex Sanders <a@snd.rs>
2019-09-07 16:32:10 +0100

    add `for()` for custom media types

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,27 @@ import {
 } from './config'
 import { fromQuery, fromUntilQuery, untilQuery } from './media-queries'
 
-type Until = { [key in Breakpoints]: string }
+type Until = {
+	[key in Breakpoints]: {
+		toString: () => string
+		for: (arg0: string) => string
+	}
+}
 
 type From = {
 	[key in Breakpoints]: {
 		toString: () => string
 		until: Until
+		for: (arg0: string) => string
 	}
 }
 
 export const until = Object.entries(breakpoints).reduce(
 	(untils, [untilName, untilWidth]) => ({
-		[untilName]: untilQuery(untilWidth),
+		[untilName]: {
+			toString: () => untilQuery(untilWidth), // this piece of freaky genius by @SiAdcock
+			for: (type: string) => untilQuery(untilWidth, type),
+		},
 		...untils,
 	}),
 	{},
@@ -27,17 +36,21 @@ export const until = Object.entries(breakpoints).reduce(
 export const from = Object.entries(breakpoints).reduce(
 	(froms, [fromName, fromWidth], i) => ({
 		[fromName]: {
+			toString: () => fromQuery(fromWidth), // this piece of freaky genius by @SiAdcock
+			for: (type: string) => fromQuery(fromWidth, type),
 			until: Object.entries(breakpoints)
 				.splice(i + 1)
 				.reduce(
 					(untils, [untilName, untilWidth], i) => ({
-						[untilName]: fromUntilQuery(fromWidth, untilWidth),
+						[untilName]: {
+							toString: () => fromUntilQuery(fromWidth, untilWidth), // this piece of freaky genius by @SiAdcock
+							for: (type: string) =>
+								fromUntilQuery(fromWidth, untilWidth, type),
+						},
 						...untils,
 					}),
 					{},
 				),
-			// this piece of freaky genius by @SiAdcock
-			toString: () => fromQuery(fromWidth),
 		},
 		...froms,
 	}),
