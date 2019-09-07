0996740f5ee1f808a9c97312e7d3f5932c861412
Alex Sanders <a@snd.rs>
2019-09-07 16:50:17 +0100

    explain in a comment

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,27 @@ import {
 } from './config'
 import { fromQuery, fromUntilQuery, untilQuery } from './media-queries'
 
+/*
+`from` and `until` are objects with overridden `toString` methods,
+so that they can be coerced into media query strings _or_ provide
+further refinement of the query. this freaky genius was thought up
+by @SiAdcock.
+
+e.g.:
+
+const styles = {
+	[from.small]: {
+		color: 'red'
+	},
+	[from.small.until.large]: {
+		color: 'blue'
+	},
+	[from.small.until.large.for('print')]: {
+		color: 'black'
+	},
+}
+*/
+
 type Until = {
 	[key in Breakpoints]: {
 		toString: () => string
@@ -25,7 +46,7 @@ type From = {
 export const until = Object.entries(breakpoints).reduce(
 	(untils, [untilName, untilWidth]) => ({
 		[untilName]: {
-			toString: () => untilQuery(untilWidth), // this piece of freaky genius by @SiAdcock
+			toString: () => untilQuery(untilWidth),
 			for: (type: string) => untilQuery(untilWidth, type),
 		},
 		...untils,
@@ -36,14 +57,14 @@ export const until = Object.entries(breakpoints).reduce(
 export const from = Object.entries(breakpoints).reduce(
 	(froms, [fromName, fromWidth], i) => ({
 		[fromName]: {
-			toString: () => fromQuery(fromWidth), // this piece of freaky genius by @SiAdcock
+			toString: () => fromQuery(fromWidth),
 			for: (type: string) => fromQuery(fromWidth, type),
 			until: Object.entries(breakpoints)
 				.splice(i + 1)
 				.reduce(
 					(untils, [untilName, untilWidth], i) => ({
 						[untilName]: {
-							toString: () => fromUntilQuery(fromWidth, untilWidth), // this piece of freaky genius by @SiAdcock
+							toString: () => fromUntilQuery(fromWidth, untilWidth),
 							for: (type: string) =>
 								fromUntilQuery(fromWidth, untilWidth, type),
 						},
