be445ab82b6112eff820a957623fd06238b0fe4a
Alex Sanders <a@snd.rs>
2019-09-11 12:19:23 +0100

    match tests to readme

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,29 +1,31 @@
 import { from, until } from './index'
 
 describe('until', () => {
-	test('is an object that provides breakpoint media queries', () => {
-		expect(`${until.small}`).toBe('@media all and (max-width: 29.9375em)')
+	test('is a map of functions that return breakpoint media queries', () => {
+		expect(until.small()).toBe('@media all and (max-width: 29.9375em)')
+	})
+
+	test('can chain a `for` map of functions that return media queries scoped by media type', () => {
+		expect(until.small.for.print()).toBe(
+			'@media print and (max-width: 29.9375em)',
+		)
 	})
 })
 
 describe('from', () => {
-	test('is a weird old beast', () => {
-		expect(`${from.small}`).toBe('@media all and (min-width: 30em)')
-		expect(`${from.small.until.medium}`).toBe(
+	test('is a map of functions that return breakpoint media queries', () => {
+		expect(from.small()).toBe('@media all and (min-width: 30em)')
+	})
+
+	test('can chain an `until` map of functions that return breakpoint media queries', () => {
+		expect(from.small.until.medium()).toBe(
 			'@media all and (min-width: 30em) and (max-width: 46.1875em)',
 		)
 	})
-})
 
-describe('custom media', () => {
-	test('can be applied to `until`', () => {
-		expect(until.small.for('print')).toBe(
-			'@media print and (max-width: 29.9375em)',
-		)
-	})
-	test('can be applied to `from`', () => {
-		expect(from.small.for('print')).toBe('@media print and (min-width: 30em)')
-		expect(from.small.until.medium.for('print')).toBe(
+	test('can chain a `for` map of functions that return media queries scoped by media type', () => {
+		expect(from.small.for.print()).toBe('@media print and (min-width: 30em)')
+		expect(from.small.until.medium.for.print()).toBe(
 			'@media print and (min-width: 30em) and (max-width: 46.1875em)',
 		)
 	})
